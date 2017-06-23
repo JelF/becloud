0618fe29633bbc0fc7c835c0ecacf84dd1f1685a
Dmitry Gubitskiy <d.gubitskiy@gmail.com>
2017-06-23 20:10:50 +0300

    Remove foreign keys before obfuscation

--- lib/becloud/obfuscation.rb
+++ lib/becloud/obfuscation.rb
@@ -2,20 +2,46 @@
 
 require 'becloud/config'
 require 'becloud/sequel'
-require 'becloud/target_preparation'
+require 'becloud/target_utils'
+
+# TODO Source db might change while obfuscating
+# TODO Rewind sequences after obfuscation
+# TODO Unique constraints
 
 module Becloud::Obfuscation
 
-  def self.obfuscate(config_path)
-    config = Becloud::Config.load_config(config_path)
-    source_db_name = config.source_db_name
+  class << self
 
-    # TODO Support all source db connection params (host, username, password, etc...)
-    Becloud::Sequel.connect(source_db_name) do |source_db|
-      target_db_name = config.target_db_name
+    # TODO Remove foreign keys before obfuscation
+    # TODO Apply foreign keys after obfuscation
+    def obfuscate(config_path)
+      config         = Becloud::Config.load_config(config_path)
+      source_db_name = config.source_db_name
 
-      Becloud::TargetPreparation.with_prepared_target(source_db_name, target_db_name) do |target_db|
-        # TODO
+      # TODO Support all source db connection params (host, username, password, etc...)
+      Becloud::Sequel.connect(source_db_name) do |source_db|
+
+        target_db_name = config.target_db_name
+        Becloud::TargetUtils.reset_target_schema(source_db_name, target_db_name)
+
+        Becloud::Sequel.connect(target_db_name) do |target_db|
+          Becloud::TargetUtils.remove_foreign_keys(target_db)
+          populate_target_db(source_db, target_db)
+        end
+      end
+    end
+
+    private
+
+    # TODO Memory usage
+    # TODO Threads
+    # TODO Ensure foreign keys are not obfuscated (both sides)
+    def populate_target_db(source_db, target_db)
+      source_db.tables.each do |table|
+        puts "Processing #{table}"
+        source_db[table].each do |row|
+          target_db[table].insert(row)
+        end
       end
     end
   end
--- lib/becloud/target_utils.rb
+++ lib/becloud/target_utils.rb
@@ -1,20 +1,28 @@
 require 'tempfile'
 require 'becloud/sequel'
 
-module Becloud::TargetPreparation
+module Becloud::TargetUtils
 
   class << self
 
     # TODO Support all args in system commands (host, username, password, etc...)
-    def with_prepared_target(source_db_name, target_db_name)
+    def reset_target_schema(source_db_name, target_db_name)
       dump_path = Tempfile.new.path
 
       run("pg_dump -s #{source_db_name} > #{dump_path}")
       run("dropdb --if-exists #{target_db_name}")
       run("createdb #{target_db_name}")
       run("psql #{target_db_name} < #{dump_path}")
+    end
 
-      Becloud::Sequel.connect(target_db_name) { |db| yield db }
+    def remove_foreign_keys(db)
+      db.tables.each do |table|
+        db.foreign_key_list(table).each do |foreign_key|
+          db.alter_table(table) do
+            drop_foreign_key(foreign_key[:columns], name: foreign_key[:name])
+          end
+        end
+      end
     end
 
     private
